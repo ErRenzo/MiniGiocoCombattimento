e93fd38a46ac16187c4488498ae0bb27234c9d83
ErRenzo <renzo.aguayoalarcon@itismajo.school>
2025-06-09 16:53:25 +0200

    MiniGiocoCombattimentoV5
    Orientamento ad oggetti del codice e inserimento nuove immagini ed eventi

--- GiocoCombattimentoJS/game.js
+++ GiocoCombattimentoJS/game.js
@@ -1,137 +1,135 @@
-// Variabili globali, vita totale e difesa attiva, gestione turni
-let turnoScelto = false;
+// Variabile globale per la gestione dei turni
 let turno = 0; // 1 = primo player, 2 = secondo player
-// Vita dei player, 100hp per entrambi i player
-let player1Hp = 100;
-let player2Hp = 100;
-let dif1 = false;
-let dif2 = false;
+// Caratteristiche dei player
+const player1 = {
+    Hp: 100,
+    dif : false, // Difesa non attiva
+};
+const player2 = {
+    Hp: 100,
+    dif : false, // Difesa non attiva
+};
 // Turno
 function Turn(){
     document.getElementById('att').hidden = false; // Mostra il bottone attacco
     document.getElementById('dif').hidden = false; // Mostra il bottone difesa
-    if(!turnoScelto)
+    document.getElementById('turnButton').hidden = true; // Nasconde il bottone del turno
+    document.getElementById('turnWho').style.fontSize = "200%"; // Dimensione del testo
+    const random = Math.floor(Math.random() * 2) + 1;
+    turno = random;
+    let stato = ""; // Stato dell'azione
+    if ( random == 1 )
     {
-        stato = ""; // Nessuno stato inizialmente
-        const random = Math.floor(Math.random() * 2) + 1;
-        turno = random;
-        if ( random == 1 )
-        {
-            document.getElementById('turnWho').innerHTML = "Turno del primo player";
-            document.getElementById('turnWho').style.color = "blue";
-        }
-        else{
-            document.getElementById('turnWho').innerHTML = "Turno del secondo player";
-            document.getElementById('turnWho').style.color = "yellow";
-        }
-        coloreTurno(); // Colore del turno attuale
-        sceltaArmaPlayer(stato); // Scelta dell'arma del player
-        turnoScelto = true;
+        document.getElementById('turnWho').innerHTML = "Turno del primo player";
+        document.getElementById('turnWho').style.color = "blue";
     }
-    else{ // Se il turno è già stato scelto
-        alert("Il turno è già stato scelto");
+    else{
+        document.getElementById('turnWho').innerHTML = "Turno del secondo player";
+        document.getElementById('turnWho').style.color = "yellow";
     }
+    coloreTurno(); // Colore del turno attuale
+    sceltaArmaPlayer(stato); // Scelta dell'arma del player
 }
 // Gioco, conseguenze del attacco e della difesa
-function btnAttacco(){
-    if(turnoScelto)
+function btnAttacco()
+{    
+    danno = Math.floor(Math.random() * 20) + 1; // Danno casuale tra 1 e 20
+    let stato = "attacco"; // Stato dell'azione
+    let dimezzatiStr = "";
+    let turnoAzione = turno; // Salva il turno attuale per la visualizzazione
+    if(turno == 1) // Primo player
     {
-        danno = Math.floor(Math.random() * 20) + 1; // Danno casuale tra 1 e 20
-        dannoDimezzato = Math.floor(danno / 2); // Danno dimezzato
-        stato = "attacco"; // Stato dell'azione
-        let dimezzati = "";
-        let turnoAzione = turno; // Salva il turno attuale per la visualizzazione
-        if(turno == 1) // Primo player
+        if(player2.dif) // Se il secondo player ha attivato la difesa
         {
-            if(dif2) // Se il secondo player ha attivato la difesa
-            {
-                player2Hp -= dannoDimezzato; // Danno dimezzato
-                dif2 = false; // Difesa disattivata
-                dimezzati = "dimezzati";
-            }
-            else
-            {
-                dimezzati = "";
-                player2Hp -= danno;
-            }
-            document.getElementById('turnWho').innerHTML = "Turno del secondo player";
-            turno = 2;
-            document.getElementById('turnWho').style.color = "yellow";
-            document.getElementById('live').innerHTML = "Il primo giocatore ha attaccato il secondo giocatore, infliggendo "+danno+" danni "+dimezzati+" !";
+            dannoDimezzato = Math.floor(danno / 2); // Danno dimezzato
+            player2.Hp -= dannoDimezzato; // Danno dimezzato
+            player2.dif = false; // Difesa disattivata
+            dimezzatiStr = "dimezzati";
+            danno = dannoDimezzato; // Danno da visualizzare
         }
-        else // Secondo player
+        else
         {
-            if(dif1) // Se il primo player ha attivato la difesa
-            {
-                player1Hp -= dannoDimezzato; // Danno dimezzato
-                dif1 = false; // Difesa disattivata
-                dimezzati = "dimezzati";
-            }
-            else
-            {
-                dimezzati = "";
-                player1Hp -= danno;
-            }
-            document.getElementById('turnWho').innerHTML = "Turno del primo player";
-            turno = 1;
-            document.getElementById('turnWho').style.color = "blue";
-            document.getElementById('live').innerHTML = "Il secondo giocatore ha attaccato il primo giocatore, infliggendo "+danno+" danni "+dimezzati+" !";
+            dimezzatiStr = "";
+            player2.Hp -= danno;
         }
-        sceltaArmaPlayer(stato, turnoAzione); // Scelta dell'arma del player
-        coloreTurno(); // Colore del turno attuale
-        vitaDinamica(); // Aggiorna la vita dinamica dei player
-        Vittoria(danno); // Controlla se uno dei due player ha vinto
+        document.getElementById('turnWho').innerHTML = "Turno del secondo player";
+        turno = 2;
+        document.getElementById('turnWho').style.color = "yellow";
+        document.getElementById('live').innerHTML = "Il primo giocatore ha attaccato il secondo giocatore, infliggendo "+danno+" danni "+dimezzatiStr+" !";
     }
-    else
+    else // Secondo player
     {
-        alert("Scegliere il turno prima di giocare!");
+        if(player1.dif) // Se il primo player ha attivato la difesa
+        {
+            dannoDimezzato = Math.floor(danno / 2); // Danno dimezzato
+            player1.Hp -= dannoDimezzato; // Danno dimezzato
+            player1.dif = false; // Difesa disattivata
+            dimezzatiStr = "dimezzati";
+            danno = dannoDimezzato; // Danno da visualizzare
+        }
+        else
+        {
+            dimezzatiStr = "";
+            player1.Hp -= danno;
+        }
+        document.getElementById('turnWho').innerHTML = "Turno del primo player";
+        turno = 1;
+        document.getElementById('turnWho').style.color = "blue";
+        document.getElementById('live').innerHTML = "Il secondo giocatore ha attaccato il primo giocatore, infliggendo "+danno+" danni "+dimezzatiStr+" !";
     }
+    sceltaArmaPlayer(stato, turnoAzione); // Scelta dell'arma del player
+    coloreTurno(); // Colore del turno attuale
+    vitaDinamica(); // Aggiorna la vita dinamica dei player
+    Vittoria(danno); // Controlla se uno dei due player ha vinto
 }
 function btnDifendi(){
-    if(turnoScelto)
+    let stato = "difesa"; // Stato dell'azione
+    let turnoAzione = turno; // Salva il turno attuale per la visualizzazione
+    if(turno == 1) // Primo player
     {
-        stato = "difesa"; // Stato dell'azione
-        let turnoAzione = turno; // Salva il turno attuale per la visualizzazione
-        if(turno == 1) // Primo player
-        {
-            dif1 = true; // Difesa attivata
-            document.getElementById('live').innerHTML = "Il primo giocatore ha scelto di difendersi!";
-            
-            document.getElementById('turnWho').innerHTML = "Turno del secondo player";
-            turno = 2;
-            document.getElementById('turnWho').style.color = "yellow";
-        }
-        else // Secondo player
-        {
-            dif2 = true; // Difesa attivata
-            document.getElementById('live').innerHTML = "Il secondo giocatore ha scelto di difendersi!";
-            
-            document.getElementById('turnWho').innerHTML = "Turno del secondo player";
-            turno = 1;
-            document.getElementById('turnWho').style.color = "blue";
-        }
-        sceltaArmaPlayer(stato, turnoAzione); // Scelta dell'arma del player
-        coloreTurno(); // Colore del turno attuale
+        player1.dif = true; // Difesa attivata
+        document.getElementById('live').innerHTML = "Il primo giocatore ha scelto di difendersi!";
+        
+        document.getElementById('turnWho').innerHTML = "Turno del secondo player";
+        turno = 2;
+        document.getElementById('turnWho').style.color = "yellow";
     }
-    else
+    else // Secondo player
     {
-        alert("Scegliere il turno prima di giocare!");
+        player2.dif = true; // Difesa attivata
+        document.getElementById('live').innerHTML = "Il secondo giocatore ha scelto di difendersi!";
+        
+        document.getElementById('turnWho').innerHTML = "Turno del primo player";
+        turno = 1;
+        document.getElementById('turnWho').style.color = "blue";
     }
+    sceltaArmaPlayer(stato, turnoAzione); // Scelta dell'arma del player
+    coloreTurno(); // Colore del turno attuale
 }
 // Vittoria e creazione button e iniziare una nuova partita
 function Vittoria(danno)
 {
-    if (player1Hp <= 0)
+    if (player1.Hp <= 0)
     {
         alert("Il secondo giocatore ha vinto! Infliggendo al nemico " + danno + " danni!");
+        // Visualizza il messaggio di vittoria
         document.getElementById('live').innerHTML = "Il secondo giocatore ha vinto! Infliggendo al nemico " + danno + " danni!";
         document.getElementById('live').style.color = "yellow"; // Colore del testo della vittoria
         document.getElementById('live').style.fontSize = "30px"; // Dimensione del testo della vittoria
         document.getElementById('live').style.height = "80%"; // Altezza del testo della vittoria
         document.getElementById('live').style.transition = "color 0.5s ease, font-size 0.5s ease"; // Transizione del testo della vittoria
+        // Nasconde il turno e i bottoni di attacco e difesa
         document.getElementById('turn').hidden = true; // Nasconde il turno
         document.getElementById('att').hidden = true; // Nasconde il bottone attacco
         document.getElementById('dif').hidden = true; // Nasconde il bottone difesa
+        // Immagine del player1 sconfitto
+        const player1Div = document.getElementById('player1');
+        const player1Sconfitto = document.createElement('img');
+        player1Sconfitto.src = "immagini/bluSconfitto.png"; // Immagine dello stato di sconfitta del primo player
+        player1Sconfitto.alt = "Sconfitta del primo player";
+        player1Div.innerHTML = ""; // Pulisce il div del primo player
+        player1Div.appendChild(player1Sconfitto);
+        // Crea il bottone per iniziare una nuova partita
         const bottone = document.createElement('button');
         bottone.style.backgroundColor = "yellow"; // Colore del bottone
         bottone.innerHTML = "Inizia una nuova partita";
@@ -140,17 +138,27 @@ function Vittoria(danno)
         // Aggiunge il bottone al div con id btnRestart
         document.getElementById('btnRestart').appendChild(bottone);
     }
-    else if (player2Hp <= 0)
+    else if (player2.Hp <= 0)
     {
         alert("Il primo giocatore ha vinto! Infliggendo al nemico " + danno + " danni!");
+        // Visualizza il messaggio di vittoria
         document.getElementById('live').innerHTML = "Il primo giocatore ha vinto! Infliggendo al nemico " + danno + " danni!";
         document.getElementById('live').style.color = "blue"; // Colore del testo della vittoria
         document.getElementById('live').style.fontSize = "30px"; // Dimensione del testo della vittoria
         document.getElementById('live').style.height = "80%" // Altezza del testo della vittoria
         document.getElementById('live').style.transition = "color 0.5s ease, font-size 0.5s ease"; // Transizione del testo della vittoria
+        // Nasconde il turno e i bottoni di attacco e difesa
         document.getElementById('turn').hidden = true; // Nasconde il turno
         document.getElementById('att').hidden = true; // Nasconde il bottone attacco
         document.getElementById('dif').hidden = true; // Nasconde il bottone difesa
+        // Immagine del player2 sconfitto
+        const player2Div = document.getElementById('player2');
+        const player2Sconfitto = document.createElement('img');
+        player2Sconfitto.src = "immagini/gialloSconfitto.png"; // Immagine dello stato di sconfitta del secondo player
+        player2Sconfitto.alt = "Sconfitta del secondo player";
+        player2Div.innerHTML = ""; // Pulisce il div del secondo player
+        player2Div.appendChild(player2Sconfitto);
+        // Crea il bottone per iniziare una nuova partita
         const bottone = document.createElement('button');
         bottone.style.backgroundColor = "blue"; // Colore del bottone
         bottone.style.color = "white"; // Colore del testo del bottone
@@ -164,10 +172,10 @@ function Vittoria(danno)
 // Visualisazione della vita restante dei player da ProgressBar
 function vitaDinamica()
 {
-    document.getElementById('hpBar1').style.width = player1Hp + '%';
-    document.getElementById('vita1').innerHTML = player1Hp + 'hp';
-    document.getElementById('hpBar2').style.width = player2Hp + '%';
-    document.getElementById('vita2').innerHTML = player2Hp + 'hp';
+    document.getElementById('hpBar1').style.width = player1.Hp + '%';
+    document.getElementById('vita1').innerHTML = player1.Hp + 'hp';
+    document.getElementById('hpBar2').style.width = player2.Hp + '%';
+    document.getElementById('vita2').innerHTML = player2.Hp + 'hp';
 }
 // Visualizzazione in colori del turno attuale
 function coloreTurno()
@@ -181,9 +189,6 @@ function coloreTurno()
 
         document.getElementById('dif').style.backgroundColor = "blue";
         document.getElementById('dif').style.color = "white";
-
-        document.getElementById('turnButton').style.backgroundColor = "blue";
-        document.getElementById('turnButton').style.color = "white";
     }
     else // Secondo player
     {
@@ -194,9 +199,6 @@ function coloreTurno()
 
         document.getElementById('dif').style.backgroundColor = "yellow";
         document.getElementById('dif').style.color = "black";
-
-        document.getElementById('turnButton').style.backgroundColor = "yellow";
-        document.getElementById('turnButton').style.color = "black";
     }
 }
 // Inizio del gioco, nasconde il bottone di attacco e difesa
@@ -210,6 +212,7 @@ function startGame()
 // Scelta in colori dellle armi e degli scudi dei player
 function sceltaArmaPlayer(stato, turnoAzione)
 {
+    let statoPersonaggio = stato; // Stato dell'azione del player
     // Crea i div per le armi e gli scudi
     const divArmi = document.getElementById('att');
     const divScudi = document.getElementById('dif');
@@ -238,9 +241,9 @@ function sceltaArmaPlayer(stato, turnoAzione)
         divScudi.innerHTML = ""; // Pulisce il div degli scudi
         divArmi.appendChild(player1Arma);
         divScudi.appendChild(player1Scudo);
-        if(stato !== "") // Se lo stato non è vuoto
+        if(statoPersonaggio !== "") // Se lo stato del personaggio non è vuoto
         {
-            azionePlayer(turnoAzione); // Crea l'immagine per l'azione del primo player
+            azionePlayer(turnoAzione, statoPersonaggio); // Crea l'immagine per l'azione del primo player
         }
     }
     else // Secondo player dopo aver fatto la azione
@@ -250,13 +253,13 @@ function sceltaArmaPlayer(stato, turnoAzione)
         divArmi.appendChild(player2Arma);
         divScudi.appendChild(player2Scudo);
 
-        if(stato !== "") // Se lo stato non è vuoto
+        if(statoPersonaggio !== "") // Se lo stato non è vuoto
         {
-            azionePlayer(turnoAzione); // Crea l'immagine per l'azione del secondo player
+            azionePlayer(turnoAzione, statoPersonaggio); // Crea l'immagine per l'azione del secondo player
         }
     }
 }
-function azionePlayer(turnoAzione)
+function azionePlayer(turnoAzione, statoPersonaggio)
 {
     if(turnoAzione == 1) // Primo player
     {
@@ -266,7 +269,7 @@ function azionePlayer(turnoAzione)
     
         player1Div.innerHTML = ""; // Pulisce il div del primo player
         
-        if(stato === "attacco") {
+        if(statoPersonaggio === "attacco") {
             player1Stato.src = "immagini/stickmanBluSpada.png"; // Immagine dello stato di attacco del primo player
             player1Stato.alt = "Attacco del primo player";
         } 
@@ -284,7 +287,7 @@ function azionePlayer(turnoAzione)
 
         player2Div.innerHTML = ""; // Pulisce il div del secondo player
 
-        if(stato === "attacco") {
+        if(statoPersonaggio === "attacco") {
             player2Stato.src = "immagini/stickmanGialloSpada.png"; // Immagine dello stato di attacco del secondo player
             player2Stato.alt = "Attacco del secondo player";
         } 
